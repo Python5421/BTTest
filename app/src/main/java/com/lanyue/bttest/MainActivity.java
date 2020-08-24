ce9a818772fcf4655c81a3ca5c9065f07aace6e7
xiexiaoyu <15024930930@163.com>
2020-08-24 10:21:33 +0800

    添加权限

--- app/src/main/java/com/lanyue/bttest/MainActivity.java
+++ app/src/main/java/com/lanyue/bttest/MainActivity.java
@@ -1,22 +1,38 @@
 package com.lanyue.bttest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        检查
+        boolean haveInstallPermission = getPackageManager().canRequestPackageInstalls();
+        if (!haveInstallPermission) {
+            startInstallPermissionSettingActivity(this);
+        }
         checkPermission();
     }
     private void checkPermission() {
+
         String[] permissions = {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -31,10 +47,33 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this, permissions, 11);
         }
     }
+
+    /**
+     * 开启设置安装未知来源应用权限界面
+     *
+     * @param context
+     */
+    public static void startInstallPermissionSettingActivity(Context context) {
+        if (context == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        //获取当前apk包URI，并设置到intent中（这一步设置，可让“未知应用权限设置界面”只显示当前应用的设置项）
+        Uri packageURI = Uri.parse("package:" + context.getPackageName());
+        intent.setData(packageURI);
+        //设置不同版本跳转未知应用的动作
+        if (Build.VERSION.SDK_INT >= 26) {
+            //intent = new Intent(android.provider.Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,packageURI);
+            intent.setAction(android.provider.Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+        } else {
+            intent.setAction(android.provider.Settings.ACTION_SECURITY_SETTINGS);
+        }
+        ((Activity) context).startActivityForResult(intent, 100);
+//        ToastUtils.showLong("请开启未知应用安装权限");
+    }
     /*
     连接蓝牙步骤：
         1.申请蓝牙权限,静态权限+动态权限检查
         2.注册广播
-
      */
 }
